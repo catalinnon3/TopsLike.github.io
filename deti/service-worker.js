421210ab178eed07e093900fc4f2561e0cd171d2
TopsLike <TopsLike@mail.ru>
2020-08-09 21:15:28 +0300

    q

--- deti/service-worker.js
+++ deti/service-worker.js
@@ -13,9 +13,7 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/deathdate/precache-manifest.09361febbfe64904cbdf499238effebb.js"
-);
+importScripts("/deathdate/precache-manifest.09361febbfe64904cbdf499238effebb.js");
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
