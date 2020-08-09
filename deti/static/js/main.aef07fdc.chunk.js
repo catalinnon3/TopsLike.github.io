08689802672d149386523407a0bd6f2c162fc6d3
TopsLike <TopsLike@mail.ru>
2020-08-09 21:43:52 +0300

    Update main.aef07fdc.chunk.js

--- deti/static/js/main.aef07fdc.chunk.js
+++ deti/static/js/main.aef07fdc.chunk.js
@@ -634,7 +634,7 @@
                             }, "\u041f\u043e\u0434\u0435\u043b\u0438\u0442\u044c\u0441\u044f \u0432 \u0438\u0441\u0442\u043e\u0440\u0438\u0438"))), this.state.snackbar)), r.a.createElement(b.h, {
                                 id: "form"
                             }, r.a.createElement(b.e, null, r.a.createElement(b.g, {
-                                top: "\u0421\u043a\u043e\u043b\u044c\u043a\u043e \u0412\u0430\u043c \u043b\u0435\u0442?",
+                                top: "тут вопрос 1?",
                                 type: "number",
                                 defaultValue: 18
                             }), r.a.createElement(b.j, {
