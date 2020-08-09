d78b7afd30ee0b183fa047fd6581c313790aef6e
TopsLike <TopsLike@mail.ru>
2020-08-10 01:58:27 +0300

    Update main.aef07fdc.chunk.js

--- deti/static/js/main.aef07fdc.chunk.js
+++ deti/static/js/main.aef07fdc.chunk.js
@@ -673,7 +673,7 @@
                                                     popout: null
                                                 }), e.go("main"), t.next = 14;
                                                 break;
-                                            case 4444:
+                                            case 11:
                                                 t.prev = 11, t.t0 = t.catch(0), e.setState({
                                                     snackbar: r.a.createElement(b.k, {
                                                         layout: "vertical",
